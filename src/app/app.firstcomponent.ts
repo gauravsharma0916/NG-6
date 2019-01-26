db7b6d0650099c67c423b991996f39333b1bf1e8
gaurav <gsonu.sharma@yahoo.co.in>
2019-01-26 03:31:24 -0800

    test

--- src/app/app.firstcomponent.ts
+++ src/app/app.firstcomponent.ts
@@ -12,4 +12,12 @@ export class FirstComponent {
     demo() {
         console.log('hello first componet called!!');
            }
+
+           temp_debuggger(data){
+              
+               let temp=data;
+               let temp1=data+5;
+
+           }
                             }
+                            
